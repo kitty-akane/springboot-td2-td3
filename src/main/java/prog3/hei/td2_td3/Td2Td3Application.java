7ab65cbc409c42f5fc08b87c30452127e6f8477f
sitraka <sitraka7011@gmail.com>
2026-03-20 11:36:37 +0300

    initial commit: setup for td2 & td3

---
+++ src/main/java/prog3/hei/td2_td3/Td2Td3Application.java
@@ -0,0 +1,13 @@
+package prog3.hei.td2_td3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td2Td3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Td2Td3Application.class, args);
+	}
+
+}
